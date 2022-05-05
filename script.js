96a270406735725ac7b7665d791c86f1de500e03
vitaminsofsalem <jokydokyduck@gmail.com>
2022-05-05 21:35:22 +0200

    Added Modal Functionality

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 'use strict';
 
+const showModalBtn = document.querySelectorAll('.show-modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+showModalBtn.forEach(btn => {
+  btn.addEventListener('click', openModal);
+});
+
+closeModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
